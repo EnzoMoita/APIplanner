20d57b3bee704b3fa37d799ddd99835ee73cc01e
Enzo Salomão <enzosadourado@hotmail.com>
2024-07-18 16:45:54 -0300

    CreateTrip With Participants

--- src/routes/create-trip.ts
+++ src/routes/create-trip.ts
@@ -15,10 +15,11 @@ export async function createTrip(app: FastifyInstance) {
                 ends_at: z.coerce.date(),
                 owner_name: z.string(),
                 owner_email: z.string().email(),
+                emails_to_invite: z.array(z.string().email()),
             })
         },
     }, async (request) => {
-        const { destination, starts_at, ends_at, owner_email, owner_name } = request.body
+        const { destination, starts_at, ends_at, owner_email, owner_name, emails_to_invite } = request.body
 
         if (dayjs(starts_at).isBefore(new Date())){
             throw new Error('Data invalida para inicio da viagem')
@@ -34,8 +35,24 @@ export async function createTrip(app: FastifyInstance) {
                 destination,
                 starts_at,
                 ends_at,
+                participants: {
+                    createMany: {
+                        data: [
+                            {
+                                name: owner_name,
+                                email: owner_email,
+                                is_owner: true,
+                                is_confirmed: true,
+                            },
+                            ...emails_to_invite.map(email => {
+                                return { email }
+                            })
+                        ],
+                    }
+                }
             }
         })
+        
 
         const mail = await getMailClient()
        const message = await mail.sendMail({

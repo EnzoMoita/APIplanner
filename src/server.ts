2fe524ca958e2064498971e00aea89d6939d42f5
Enzo Salomão <enzosadourado@hotmail.com>
2024-07-21 16:57:14 -0300

    NewsRoutesForLinks and Activities

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,9 @@ import { confirmTrip } from './routes/confirm-trip';
 import cors from '@fastify/cors'
 import { confirmParticipants } from './routes/confirm-participant';
 import { createActivity } from './routes/create-activity';
+import { getActivities } from './routes/get-activities';
+import { createLink } from './routes/create-link';
+import { getLinks } from './routes/get-links';
 
 const app = fastify()
 
@@ -19,6 +22,9 @@ app.register(createTrip)
 app.register(confirmTrip)
 app.register(confirmParticipants)
 app.register (createActivity)
+app.register(getActivities)
+app.register(createLink)
+app.register(getLinks)
 
 app.listen({ port: 3333 }).then(() => {
     console.log('Server Running in port 3333')
